a69dc3a71bb6576fab9c2b8ce6a3d8e0aca68f30
Nandam Josh Nihanth <160224150+joshnandam@users.noreply.github.com>
2025-01-25 16:26:07 +0530

    Update mergsort.java

--- mergsort.java
+++ mergsort.java
@@ -44,7 +44,7 @@ class merge {
 		int arr[]=new int[n];
 		b=new int[n];
 		System.out.println("Enter the elements");
-		for(int i=0; i<=n; i++){
+		for(int i=0; i<n; i++){
 			arr[i]=sc.nextInt(n);
 		}
 		merge(arr,0,arr.length-1);
